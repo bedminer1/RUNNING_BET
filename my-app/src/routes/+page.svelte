c8a280236aa7e2bd90f4a17839dd037a8a696906
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-07-23 15:09:41 +0800

    finished the basic logic for the calculator, TODO: add tracking to a static file

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,15 +1,25 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	let myInput = ""
+	let herInput = ""
+	$: scoreNeeded = calculateCutoff(herInput)
+	$: winForMe = Number(myInput) > scoreNeeded
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
+	function calculateCutoff(input: string): number {
+		let herScore: number = Number(input)
+		if (herScore < 5) {
+			return herScore * 2
+		} else if (herScore < 10) {
+			// 10 + (herScore - 5) * 1.5
+			return 2.5 + (1.5 * herScore)
+		} else {
+			// 17.5 + (herScore - 10)
+			return 7.5 + herScore
+		}
+	}
+</script>
+
+<div>
+	<input type="text" bind:value={myInput} class="input">
+	<input type="text" bind:value={herInput} class="input">
+	{winForMe}
 </div>
