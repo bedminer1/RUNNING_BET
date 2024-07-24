b865ee7d2dd73da7aefd1da17a0b0f6753981d8c
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-07-24 11:51:26 +0800

    did up the ui to be less eyerape

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -18,8 +18,17 @@
 	}
 </script>
 
-<div>
-	<input type="text" bind:value={myInput} class="input">
-	<input type="text" bind:value={herInput} class="input">
-	{winForMe}
+<div class="flex flex-col h-screen items-center justify-center">
+	<div class="w-1/4 flex flex-col gap-3 mb-5">
+		<input type="text" bind:value={myInput} class="input" placeholder="Alex's score">
+		<input type="text" bind:value={herInput} class="input" placeholder="Yunna's score">
+		<p class="pl-2">Distance needed for Alex: {scoreNeeded.toFixed(2)}</p>
+	</div>
+	{#if !myInput.length || !herInput.length}
+		<p>Please input scores</p>
+	{:else if winForMe}
+		<p>Alex wins</p>
+	{:else}
+		<p>Yuuna wins</p>
+	{/if}
 </div>
