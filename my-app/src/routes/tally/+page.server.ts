bee23b50e9615e98b8bdc2304f53f31fe2dc01d3
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-08-05 16:45:49 +0800

    logic to calculate different multiplier schemes depending on the scheme at the time

--- my-app/src/routes/tally/+page.server.ts
+++ my-app/src/routes/tally/+page.server.ts
@@ -1,7 +1,7 @@
 import fs from "fs"
 import path from "path"
 
-// calculating previous week's records
+// takes in raw scores from txt and return arr of numbers containing scores for each week
 function calculateScores(input: string) {
   let res = []
   for (let line of input.split("\n")) {
@@ -15,7 +15,67 @@ function calculateScores(input: string) {
   return res;
 }
 
-const filePath = path.resolve("src/lib/alexScores.txt")
-let input = fs.readFileSync(filePath, "utf-8")
-let res = calculateScores(input)
-console.log(res)
+// variables to access the txt files containing the scores
+const filePathAlex = path.resolve("src/lib/alexScores.txt")
+const filePathYoona = path.resolve("src/lib/yoonaScores.txt")
+const inputAlex = fs.readFileSync(filePathAlex, "utf-8")
+const inputYoona = fs.readFileSync(filePathYoona, "utf-8")
+const resAlex = calculateScores(inputAlex)
+const resYoona = calculateScores(inputYoona)
+
+// returns an array of RawWeeklyRecord with the score at the end
+function parseRecords(alexScores: string[], yoonaScores: string[]) {
+    if (alexScores.length !== yoonaScores.length) return "Lengths are not equal"
+    let res = []
+    let alexScore = 0
+    let yoonaScore = 0
+
+    for (let i = 0; i < alexScores.length; i++) {
+        let newRecord: RawWeeklyRecord = {
+            myScore: Number(alexScores[i]),
+            herScore: Number(yoonaScores[i]),
+            neededScore: calculateDifferentCutoffs(Number(yoonaScores[i]), i + 1),
+            winForMe: Number(alexScores[i]) > calculateDifferentCutoffs(Number(yoonaScores[i]), i + 1)
+        }
+        if (Number(alexScores[i]) > Number(yoonaScores[i]) * 2) alexScore++
+        else yoonaScore++
+        res.push(newRecord)
+    }
+    res.push(`SCORE: ${alexScore} - ${yoonaScore}`)
+    
+    return res
+}
+
+// returns different cutoffs depending on the multiplier scheme at the time
+function calculateDifferentCutoffs(herScore: number, weekId: number) {
+  let multiplier
+
+  // before 1APR
+  if (weekId <= 10) multiplier = (ele: number)=> 2 * ele
+  // between 1APR and 16JUN
+  else if (weekId <= 21) multiplier = (ele: number)=> 1.5 * ele
+  // before 22JUL
+  else if (weekId <= 26) multiplier = (ele: number)=> 1 * ele
+  // after 22JUL
+  else multiplier = calculateCurrentCutoff
+
+  return Number(multiplier(herScore).toFixed(2))
+}
+
+// calculateCutoff takes in Yoona's score and 
+// returns the score needed to beat her for the current tiered scheme
+function calculateCurrentCutoff(input: number|undefined): number {
+  let herScore: number = Number(input) ?? 0
+  if (herScore < 5) {
+    return herScore * 2
+  } else if (herScore < 10) {
+    // 10 + (herScore - 5) * 1.5
+    return 2.5 + (1.5 * herScore)
+  } else {
+    // 17.5 + (herScore - 10)
+    return 7.5 + herScore
+  }
+}
+
+let records = parseRecords(resAlex, resYoona)
+console.log(records, "hw")
