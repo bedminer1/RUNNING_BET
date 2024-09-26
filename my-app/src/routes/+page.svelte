13350942c16b646bfe7fc91d6582d222f96c4290
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-09-26 23:57:05 +0800

    added ui to add intervals n remove intervals. TODO: save to file

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import { superForm } from 'sveltekit-superforms'
 	import { Paginator, getToastStore } from '@skeletonlabs/skeleton'
+	import { CalculateCutoff } from '$lib/calculate.js';
 
 	export let data
 
 	const { form, errors, message } = superForm(data.form)
 
 	$form.weekID = data.records.length + 1
-	$: $form.neededScore = $form.herScore === undefined ? undefined : Number(calculateCutoff($form.herScore).toFixed(2))
+	$: $form.neededScore = $form.herScore === undefined ? undefined : Number(CalculateCutoff($form.herScore, [[5,2], [10,1.5]]).toFixed(2))
 	$: $form.winForMe = $form.myScore! > $form.neededScore!
 	$: if ($form.winForMe) {
 		$form.myPoints = data.records.at(-1)!.myPoints + 1
@@ -54,40 +55,54 @@
 		paginationSettings.page * paginationSettings.limit + paginationSettings.limit
 	)
 
-	// calculateCutoff takes in Yoona's score and returns the score needed to beat her
-	function calculateCutoff(input: number|undefined): number {
-		let herScore: number = Number(input) ?? 0
-		if (herScore < 5) {
-			return herScore * 2
-		} else if (herScore < 10) {
-			// 10 + (herScore - 5) * 1.5
-			return 2.5 + (1.5 * herScore)
-		} else {
-			// 17.5 + (herScore - 10)
-			return 7.5 + herScore
-		}
+	// Configure scheme
+	let scheme: number[][] = [[5,2], [10,1.5]]
+	$: displayScheme = JSON.stringify(scheme)
+	let distance: string
+	let multiplier: string
+
+	function addInterval(distance: string, multiplier: string) {
+		scheme = [...scheme, [Number(distance), Number(multiplier)]]
+		scheme.sort((a,b) => a[0] - b[0])
 	}
+
+	function deleteInterval() {
+		scheme.pop()
+		scheme = scheme
+	}
+
 </script>
 
 <div class="flex flex-col h-screen items-center justify-center">
 <!-- FORM -->
-	<form class="w-1/4 flex flex-col mb-10 justify-center items-center gap-3" action="?/saveRecord" method="POST">
-		<div class="flex flex-col w-full">
-			{#if $errors.myScore}<span class="invalid italic m-0 p-0 text-xs text-error-400 ml-2 w-full">{$errors.myScore}</span>{/if}
-			<input name="myScore" type="text" bind:value={$form.myScore} class="input mt-0" placeholder="Alex's score">
+ <div class="w-full flex gap-10 justify-center">
+	 <form class="w-1/4 flex flex-col mb-10 justify-center items-center gap-3" action="?/saveRecord" method="POST">
+		 <div class="flex flex-col w-full">
+			 {#if $errors.myScore}<span class="invalid italic m-0 p-0 text-xs text-error-400 ml-2 w-full">{$errors.myScore}</span>{/if}
+			 <input name="myScore" type="text" bind:value={$form.myScore} class="input mt-0" placeholder="Alex's score">
+		 </div>
+		 
+		 <div class="flex flex-col w-full">
+			 {#if $errors.herScore}<span class="invalid italic m-0 p-0 text-xs text-error-400 ml-2 w-full">{$errors.herScore}</span>{/if}
+			 <input name="herScore" type="text" bind:value={$form.herScore} class="input mt-0" placeholder="윤아's score">
+		 </div>
+		 <input name="neededScore" type="text" bind:value={$form.neededScore} class="input" placeholder="Distance cutoff">
+		 <input name="weekID" type="hidden" bind:value={$form.weekID}>
+		 <input name="myPoints" type="hidden" bind:value={$form.myPoints}>
+		 <input name="herPoints" type="hidden" bind:value={$form.herPoints}>
+		 <input name="winForMe" type="hidden" bind:value={$form.winForMe}>
+		 <button class="btn variant-ghost-primary w-1/2 rounded-md mt-4 text-wheat-500">Save Result</button>
+	 </form>
+	 <form class="w-1/4 flex flex-col mb-10 justify-center items-center gap-3">
+		<input name="neededScore" type="text" bind:value={distance} class="input" placeholder="Distance Upper Bound">
+		<input name="neededScore" type="text" bind:value={multiplier} class="input" placeholder="Multiplier">
+		<input class="input h-12" disabled placeholder="Scheme" bind:value={displayScheme}>
+		<div class="flex gap-2">
+			<button on:click={() => addInterval(distance, multiplier)} class="btn variant-ghost-primary w-1/2 rounded-md mt-4 text-wheat-500">Add</button>
+			<button on:click={() => deleteInterval()} class="btn variant-ghost-primary w-1/2 rounded-md mt-4 text-wheat-500">Remove</button>
 		</div>
-		
-		<div class="flex flex-col w-full">
-			{#if $errors.herScore}<span class="invalid italic m-0 p-0 text-xs text-error-400 ml-2 w-full">{$errors.herScore}</span>{/if}
-			<input name="herScore" type="text" bind:value={$form.herScore} class="input mt-0" placeholder="윤아's score">
-		</div>
-		<input name="neededScore" type="text" bind:value={$form.neededScore} class="input" placeholder="Distance cutoff">
-		<input name="weekID" type="hidden" bind:value={$form.weekID}>
-		<input name="myPoints" type="hidden" bind:value={$form.myPoints}>
-		<input name="herPoints" type="hidden" bind:value={$form.herPoints}>
-		<input name="winForMe" type="hidden" bind:value={$form.winForMe}>
-		<button class="btn variant-ghost-primary w-1/2 rounded-md mt-4 text-wheat-500">Save Result</button>
-	</form>
+	 </form>
+ </div>
 
 <!-- TABLE -->
 <div class="table-container w-1/2">
