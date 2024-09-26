7d0bc4acb20261ce2dbd96c5ac9402b9b8e9f5eb
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-09-26 15:34:37 +0800

    refactored and added test cases

--- my-app/src/lib/calculate.js
+++ my-app/src/lib/calculate.js
@@ -1,49 +1,82 @@
-var testCases = [
-    {
-        herScore: 2,
-        scheme: [[1, 2]],
-        expRes: 3
-    },
-    {
-        herScore: 2,
-        scheme: [[1, 2], [2, 3]],
-        expRes: 5
-    },
-    {
-        herScore: 19,
-        scheme: [[5, 2], [10, 1.5]],
-        expRes: 26.5
-    }
-];
+// CalculateCutoff takes in the scheme and score and calculate the score I would need to win
 function CalculateCutoff(herScore, scheme) {
     var res = 0;
     // go through all the schemes 
     for (var i = 0; i < scheme.length; i++) {
-        var s = scheme[i];
-        // in the case where herScore runs is less than scheme afford
-        if (herScore <= s[0]) {
-            res += herScore * s[1];
-            herScore = 0;
-            break;
+        var _a = scheme[i], dist = _a[0], mult = _a[1];
+        var prevDist = i > 0 ? scheme[i - 1][0] : 0;
+        var range = dist - prevDist;
+        // in the case where herScore is less than scheme's total coverage
+        if (herScore <= range) {
+            res += herScore * mult;
+            return res;
         }
-        if (i === 0) {
-            res += s[0] * s[1];
-            herScore -= s[0];
-        }
-        else {
-            res += (s[0] - scheme[i - 1][0]) * s[1];
-            herScore -= (s[0] - scheme[i - 1][0]);
-        }
+        res += range * mult;
+        herScore -= range;
     }
-    // if there is leftover just add
+    // if there is leftover just add with 1x multiplier
     res += herScore;
     return res;
 }
-function testCalculateCutoff() {
+var testCases = [
+    {
+        name: "herScoreLessThanCoverage",
+        herScore: 5,
+        scheme: [[10, 2]],
+        expRes: 10
+    },
+    {
+        name: "herScoreEqualToCoverage",
+        herScore: 12,
+        scheme: [[3, 2], [8, 1.5], [12, 3]],
+        expRes: 25.5
+    },
+    {
+        name: "herScoreMoreThanCoverage",
+        herScore: 19,
+        scheme: [[5, 2], [10, 1.5]],
+        expRes: 26.5
+    },
+    {
+        name: "noSchemeOnlyBaseMultiplier",
+        herScore: 7,
+        scheme: [],
+        expRes: 7
+    },
+    {
+        name: "herScoreZero",
+        herScore: 0,
+        scheme: [[5, 2], [10, 3]],
+        expRes: 0
+    },
+    {
+        name: "largeScoreMultipleSchemes",
+        herScore: 50,
+        scheme: [[10, 2], [20, 3], [30, 1.5]],
+        expRes: 85
+    },
+];
+function testCalculateCutoff(verbose) {
+    var testsPassed = 0;
     for (var _i = 0, testCases_1 = testCases; _i < testCases_1.length; _i++) {
         var tc = testCases_1[_i];
         var res = CalculateCutoff(tc.herScore, tc.scheme);
-        console.log("output:", res, "expected:", tc.expRes);
+        var testPassed = tc.expRes === res;
+        // if not verbose skip
+        if (!verbose) {
+            if (testPassed)
+                testsPassed++;
+            continue;
+        }
+        // if verbose, log it out
+        console.log("Test:", tc.name);
+        if (tc.expRes === res) {
+            console.log("Test Status: Passed");
+            continue;
+        }
+        console.log("Test Status: Failed");
+        console.log("Expected:", tc.expRes, "Output:", res);
     }
+    console.log(testsPassed, "/", testCases.length, "tests passed");
 }
-testCalculateCutoff();
+testCalculateCutoff(false); // verbose = false
--- my-app/src/lib/calculate.ts
+++ my-app/src/lib/calculate.ts
@@ -4,29 +4,27 @@ function CalculateCutoff(herScore: number, scheme: number[][]): number {
 
     // go through all the schemes 
     for (let i = 0; i < scheme.length; i++) {
-        let s = scheme[i]
-        // in the case where herScore runs is less than scheme afford
-        if (herScore <= s[0]) {
-            res += herScore * s[1]
-            herScore = 0
-            break
+        const [dist, mult] = scheme[i]
+        const prevDist = i > 0 ? scheme[i-1][0] : 0
+        const range = dist - prevDist
+        // in the case where herScore is less than scheme's total coverage
+        if (herScore <= range) {
+            res += herScore * mult
+            return res
         }
-        if (i === 0) {
-            res += s[0] * s[1]
-            herScore -= s[0]
-        } else {
-            res += (s[0] - scheme[i-1][0]) * s[1]
-            herScore -= (s[0] - scheme[i-1][0])
-        }
+        
+        res += range * mult
+        herScore -= range
     }
 
-    // if there is leftover just add
+    // if there is leftover just add with 1x multiplier
     res += herScore
     return res
 }
 
 // FOR TESTING
 interface testCase {
+    name: string,
     herScore: number,
     scheme: number[][] // [distance, multiplier]
     expRes: number,
@@ -34,27 +32,66 @@ interface testCase {
 
 const testCases: testCase[] = [
     {
-        herScore: 2,
-        scheme: [[1,2]],
-        expRes: 3,
+        name: "herScoreLessThanCoverage",
+        herScore: 5,
+        scheme: [[10,2]],
+        expRes: 10,
     },
     {
-        herScore: 2,
-        scheme: [[1,2], [2,3]],
-        expRes: 5,
+        name: "herScoreEqualToCoverage",
+        herScore: 12,
+        scheme: [[3, 2], [8, 1.5], [12, 3]],
+        expRes: 25.5, // 3*2 + 5*1.5 + 4*3
     },
     {
+        name: "herScoreMoreThanCoverage",
         herScore: 19,
         scheme: [[5,2],[10,1.5]],
         expRes: 26.5,
-    }
+    },
+    {
+        name: "noSchemeOnlyBaseMultiplier",
+        herScore: 7,
+        scheme: [],
+        expRes: 7, // No scheme, just 1x multiplier for herScore
+    },
+    {
+        name: "herScoreZero",
+        herScore: 0,
+        scheme: [[5, 2], [10, 3]],
+        expRes: 0, // herScore is 0
+    },
+    {
+        name: "largeScoreMultipleSchemes",
+        herScore: 50,
+        scheme: [[10, 2], [20, 3], [30, 1.5]],
+        expRes: 85, // 10*2 + 10*3 + 10*1.5 + remaining 20*1
+    },
 ]
 
-function testCalculateCutoff() {
+function testCalculateCutoff(verbose: boolean) {
+    let testsPassed = 0
     for (let tc of testCases) {
         let res = CalculateCutoff(tc.herScore, tc.scheme)
-        console.log("output:", res, "expected:", tc.expRes)
+        let testPassed = tc.expRes === res
+
+        // if not verbose skip
+        if (!verbose) {
+            if (testPassed) testsPassed++
+            continue
+        }
+
+        // if verbose, log it out
+        console.log("Test:", tc.name)
+        if (tc.expRes === res) {
+            console.log("Test Status: Passed")
+            continue
+        }
+        console.log("Test Status: Failed")
+        console.log("Expected:", tc.expRes, "Output:", res)
     }
+
+    console.log(testsPassed, "/", testCases.length, "tests passed")
 }
 
-testCalculateCutoff()
+testCalculateCutoff(false) // verbose = false
