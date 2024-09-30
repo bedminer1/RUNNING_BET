fe0ebe2a92c2964e064166dfe914b4c3d5b5f621
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-09-30 16:56:59 +0800

    calculateScore ported from js implementation

--- running_bet_cli/api/actions.go
+++ running_bet_cli/api/actions.go
@@ -14,7 +14,30 @@ type Record struct {
 type Records []Record
 
 func calculateScore(herScore float32, scheme [][]float32) float32 {
-	return 1
+	var res float32 = 0
+
+	for i := range scheme {
+		dist := scheme[i][0]
+		mult := scheme[i][1]
+		var prevDist float32
+
+		if i > 0 {
+			prevDist = scheme[i-1][0]
+		} else {
+			prevDist = 0
+		}
+		span := dist - prevDist
+		if herScore <= span {
+			res += herScore * mult
+			return res
+		}
+
+		res += span * mult
+		herScore -= span
+	}
+
+	res += herScore
+	return res
 }
 
 func (db *Records) Add(myScore, herScore float32, scheme [][]float32) error {
