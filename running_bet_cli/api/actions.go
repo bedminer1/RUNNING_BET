3b1b51b49b2ca20e2ede58221891886d27de9c6c
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-09-30 16:48:39 +0800

    Add implemented for cli

--- running_bet_cli/api/actions.go
+++ running_bet_cli/api/actions.go
@@ -8,5 +8,37 @@ type Record struct {
 	winForMe    bool
 	myPoints    int
 	herPoints   int
+	scheme      [][]float32
 }
 
+type Records []Record
+
+func calculateScore(herScore float32, scheme [][]float32) float32 {
+	return 1
+}
+
+func (db *Records) Add(myScore, herScore float32, scheme [][]float32) error {
+	neededScore := calculateScore(herScore, scheme)
+	myPoints := (*db)[len(*db)-1].myPoints + 1
+	herPoints := (*db)[len(*db)-1].herPoints + 1
+
+
+	if myScore > neededScore {
+		myPoints++
+	} else {
+		herPoints++
+	}
+	
+	r := Record{
+		weekID: len(*db),
+		myScore: myScore,
+		herScore: herScore,
+		neededScore: neededScore,
+		winForMe: myScore > neededScore,
+		myPoints: myPoints,
+		herPoints: herPoints,
+		scheme: scheme,
+	}
+	*db = append(*db, r)
+	return nil
+}
