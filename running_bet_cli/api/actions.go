93cc65c8a753978382a4ef8c64e5315817e0b5ea
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-09-30 14:19:41 +0800

    init golang cli version

---
+++ running_bet_cli/api/actions.go
@@ -0,0 +1,12 @@
+package api
+
+type Record struct {
+	weekID      int
+	myScore     float32
+	herScore    float32
+	neededScore float32
+	winForMe    bool
+	myPoints    int
+	herPoints   int
+}
+
---
+++ running_bet_cli/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/bedminer1/running_bet/cmd"
+
+func main() {
+	cmd.Execute()
+}
