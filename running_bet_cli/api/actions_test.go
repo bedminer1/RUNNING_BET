8cd072b92def36151eeb2c0341279ac072218ccc
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-10-01 17:34:16 +0800

    testing json unmarshalling

--- running_bet_cli/api/actions_test.go
+++ running_bet_cli/api/actions_test.go
@@ -1,7 +1,6 @@
 package api_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/bedminer1/running_bet/api"
@@ -9,8 +8,8 @@ import (
 
 func TestGetTxt(t *testing.T) {
 	expData := api.Record{
-		MyScore:     float32(36.85),
-		WinForMe:    true,
+		MyScore:  float32(36.85),
+		WinForMe: true,
 	}
 
 	db := &api.Records{}
@@ -31,10 +30,34 @@ func TestGetTxt(t *testing.T) {
 	}
 }
 
+func TestGetJSON(t *testing.T) {
+	expData := api.Record{
+		MyScore:  float32(36.85),
+		WinForMe: true,
+	}
+
+	db := &api.Records{}
+	fileDirectory := "../local_storage/"
+	fileName := "record_test"
+	fileType := "json"
+	if err := db.Get(fileDirectory, fileName, fileType); err != nil {
+		t.Errorf("Unexpected error: %q", err)
+	}
+	res := (*db)[0]
+
+	if expData.MyScore != res.MyScore {
+		t.Errorf("Expected score: %f, Recorded Score: %f", expData.MyScore, res.MyScore)
+	}
+
+	if expData.WinForMe != res.WinForMe {
+		t.Errorf("Expected WinForMe: %t, Recorded WinForMe: %t", expData.WinForMe, res.WinForMe)
+	}
+}
+
 func TestSave(t *testing.T) {
 	db := &api.Records{}
 	fileDirectory := "../local_storage/"
-	fileName := "record"
+	fileName := "record_test"
 	fileType := "txt"
 	if err := db.Get(fileDirectory, fileName, fileType); err != nil {
 		t.Error("Unexpected error getting data")
@@ -52,7 +75,6 @@ func TestSave(t *testing.T) {
 		}
 	}
 
-	fmt.Println(*db)
 	if err := db.Save(fileDirectory, fileName); err != nil {
 		t.Errorf("Unexpected error saving file: %q", err)
 	}
